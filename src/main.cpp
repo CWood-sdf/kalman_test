7e5bca1fcd831f9aafdae7a56269e11b9fdeac31
CWood-sdf <woodchristophermichael@gmail.com>
2023-11-19 07:08:22 -0500

    ok we added some autodiff capabilities so that we can do jacobian soon

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,151 @@
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <optional>
+#include <queue>
 #include <random>
+#include <stack>
+#include <unordered_set>
 #include <vector>
 using namespace std;
 
+template <class T> class ReverseTopologicalSort {
+public:
+	static std::vector<std::shared_ptr<T>>
+	reverseTopoSort(const std::shared_ptr<T>& startNode) {
+		std::vector<std::shared_ptr<T>> result;
+		std::unordered_set<std::shared_ptr<T>> visited;
+		std::stack<std::shared_ptr<T>> stack;
+
+		reverseTopoSortUtil(startNode, visited, stack);
+
+		while (!stack.empty()) {
+			result.push_back(stack.top());
+			stack.pop();
+		}
+
+		return result;
+	}
+
+private:
+	static void reverseTopoSortUtil(
+		const std::shared_ptr<T>& node,
+		std::unordered_set<std::shared_ptr<T>>& visited,
+		std::stack<std::shared_ptr<T>>& stack
+	) {
+		visited.insert(node);
+
+		for (const auto& parent : node->inputs) {
+			if (visited.find(parent) == visited.end()) {
+				reverseTopoSortUtil(parent, visited, stack);
+			}
+		}
+
+		stack.push(node);
+	}
+};
+template <typename T> struct BackwardAutoDiff {
+	typedef BackwardAutoDiff<T> Self;
+	enum OpTp {
+		Const,
+		Add,
+		// Sub,
+		Mul,
+		// Div,
+	};
+	T constInput;
+	vector<std::shared_ptr<BackwardAutoDiff<T>>> inputs;
+	std::optional<T> output;
+	const OpTp op;
+	std::optional<T> gradient;
+	BackwardAutoDiff(OpTp op) : op(op) {
+	}
+	BackwardAutoDiff(T val) : op(Const), constInput(val) {
+	}
+	static std::shared_ptr<Self> makeConst(T val) {
+		auto ret = std::make_shared<Self>(Const);
+		ret->constInput = val;
+		return ret;
+	}
+	friend std::shared_ptr<Self>
+	operator+(std::shared_ptr<Self> a, std::shared_ptr<BackwardAutoDiff> b) {
+		auto ret = std::make_shared<Self>(Add);
+		ret->inputs.push_back(a);
+		ret->inputs.push_back(b);
+		return ret;
+	}
+	friend std::shared_ptr<Self>
+	operator*(std::shared_ptr<Self> a, std::shared_ptr<BackwardAutoDiff> b) {
+		auto ret = std::make_shared<Self>(Mul);
+		ret->inputs.push_back(a);
+		ret->inputs.push_back(b);
+		return ret;
+	}
+
+	void forward() {
+		if (this->output.has_value()) {
+			return;
+		}
+		switch (this->op) {
+		case Const:
+			this->output = this->constInput;
+			break;
+		case Add:
+			this->inputs[0]->forward();
+			this->inputs[1]->forward();
+			this->output = this->inputs[0]->output.value() +
+			               this->inputs[1]->output.value();
+			break;
+		case Mul:
+			this->inputs[0]->forward();
+			this->inputs[1]->forward();
+			this->output = this->inputs[0]->output.value() *
+			               this->inputs[1]->output.value();
+			break;
+		}
+	}
+	static void backward(std::shared_ptr<Self> node) {
+		auto reversed = ReverseTopologicalSort<Self>::reverseTopoSort(node);
+		for (std::shared_ptr<Self>& node : reversed) {
+			for (std::shared_ptr<Self>& input : node->inputs) {
+				if (!input->gradient.has_value()) {
+					input->gradient = 0;
+				}
+				input->gradient.value();
+			}
+			if (!node->gradient.has_value()) {
+				node->gradient = 0;
+			}
+			switch (node->op) {
+			case Const:
+				break;
+			case Add:
+				node->inputs[0]->gradient.value() += node->gradient.value();
+				node->inputs[1]->gradient.value() += node->gradient.value();
+				break;
+			case Mul:
+				node->inputs[0]->gradient.value() +=
+					node->gradient.value() * node->inputs[1]->output.value();
+				node->inputs[1]->gradient.value() +=
+					node->gradient.value() * node->inputs[0]->output.value();
+				break;
+			}
+		}
+	}
+};
+
+// int main() {
+// 	auto x = BackwardAutoDiff<double>::makeConst(2);
+// 	auto y = BackwardAutoDiff<double>::makeConst(3);
+// 	auto z = x * x + x * y;
+// 	z->forward();
+// 	z->gradient = 1;
+// 	BackwardAutoDiff<double>::backward(z);
+// 	cout << z->output.value() << endl;
+// 	cout << x->gradient.value() << endl;
+// 	cout << y->gradient.value() << endl;
+// }
 const size_t state_size = 3;
 const size_t control_size = 1;
 const size_t measurement_size = 2;
@@ -170,35 +311,22 @@ std::vector<TrajectoryPoint> generate_rocket_trajectory(
 int main() {
 	cout << std::fixed;
 	cout << std::setprecision(2);
-	auto shutoffT = 2;
+	auto shutoffT = 10;
 	auto shutoffRate = 0.1;
-	auto motorForce = 60;
+	auto motorForce = 70;
 	auto trajectory = generate_rocket_trajectory(
-		0.00001, motorForce, shutoffT, 1.8, 0.1, 0, shutoffRate
+		0.00001, motorForce, shutoffT, 29.8, 0.1, 0, shutoffRate
 	);
 
-	// vector<double> altitudes = {-32.4, -11.1, 18, 22.9,
-	// 	19.5, 28.5, 46.5, 68.9, 48.2, 56.1, 90.5, 104.9,
-	// 	140.9, 148, 187.6, 209.2, 244.6, 276.4, 323.5,
-	// 	357.3, 357.4, 398.3, 446.7, 465.1, 529.4, 570.4,
-	// 	636.8, 693.3, 707.3, 748.5};
-	//
-	// vector<double> accelerations = {39.72, 40.02, 39.97,
-	// 	39.81, 39.75, 39.6, 39.77, 39.83, 39.73, 39.87,
-	// 	39.81, 39.92, 39.78, 39.98, 39.76, 39.86, 39.61,
-	// 	39.86, 39.74, 39.87, 39.63, 39.67, 39.96, 39.8,
-	// 	39.89, 39.85, 39.9, 39.81, 39.81, 39.68};
-	// trajectory = {};
-	// for (int i = 0; i < altitudes.size(); i++) {
-	// 	trajectory.push_back(TrajectoryPoint(altitudes[i],
-	// 		altitudes[i], 0, 0, accelerations[i]));
-	// }
-
 	int i = 0;
-	// Q = F * Q_base * F.transpose();
-	//
+	// since we're assuming the jerk is constant, we base the covariances off of
+	// that
+
+	// Position is the third integral of jerk
 	double jerk_to_pos = std::pow(dt, 3) / 6;
+	// Velocity is the second integral of jerk
 	double jerk_to_vel = std::pow(dt, 2) / 2;
+	// Acceleration is the first integral of jerk
 	double jerk_to_acc = dt;
 	double v_x = jerk_to_pos * jerk_to_pos;
 	double v_v = jerk_to_vel * jerk_to_vel;
@@ -206,17 +334,21 @@ int main() {
 	double v_xv = jerk_to_pos * jerk_to_vel;
 	double v_xa = jerk_to_pos * jerk_to_acc;
 	double v_va = jerk_to_vel * jerk_to_acc;
+	// the number multiplying Q is the only random number in the whole program
+	// it is the standard deviation of jerk
 	Q = Eigen::Matrix<double, state_size, state_size>({
 			{ v_x, v_xv, v_xa},
             {v_xv,  v_v, v_va},
             {v_xa, v_va,  v_a}
     }) *
-	    pow(3.7, 2);
+	    pow(2.7, 2);
 	auto control = Eigen::Vector<double, control_size>({ { 0 } });
 	Eigen::Vector<double, state_size> predicted_state =
 		F * state + G * control + w;
 	Eigen::Matrix<double, state_size, state_size> predicted_P =
 		F * P * F.transpose() + Q;
+	double meanOfSquErr = 0;
+	double meanOfMeasErr = 0;
 	for (auto& point : trajectory) {
 		// get our control input
 		Eigen::Vector<double, control_size> control;
@@ -242,6 +374,10 @@ int main() {
 		state =
 			predicted_state + kalman_gain * (measurement - H * predicted_state);
 
+		auto err = state(0) - point.actual_altitude;
+		meanOfSquErr += err * err;
+		auto measErr = point.measured_altitude - point.actual_altitude;
+		meanOfMeasErr += measErr * measErr;
 		// update the state covariance matrix
 		auto predict_p_help =
 			(Eigen::Matrix<double, state_size, state_size>::Identity() -
@@ -257,6 +393,12 @@ int main() {
 		trajectory[i].kalman_stddev = sqrt(P(0, 0));
 		i++;
 	}
+	cout << "Mean of Squared Error: " << meanOfSquErr / trajectory.size()
+		 << endl;
+	cout << "Mean of Measurement Error: " << meanOfMeasErr / trajectory.size()
+		 << endl;
+	cout << "Kalman improvement: " << 100 - meanOfSquErr / meanOfMeasErr * 100
+		 << "%" << endl;
 
 	auto file = ofstream();
 	file.open("./graph/js/data.js");
@@ -282,4 +424,37 @@ int main() {
 		cout << "Unable to open file";
 	}
 	file.close();
+
+	// typedef std::shared_ptr<BackwardAutoDiff<double>> ADP;
+	// typedef BackwardAutoDiff<double> AD;
+	//
+	// auto x2 = AD::makeConst(2);
+	// auto y2 = AD::makeConst(3);
+	// auto x1 = AD::makeConst(3);
+	// auto y1 = AD::makeConst(4);
+	//
+	// Eigen::Matrix<ADP, 2, 2> A = Eigen::Matrix<ADP, 2, 2>({
+	// 	{x1, y1},
+	//        {x2, y2}
+	//    });
+	//
+	// auto z1 = AD::makeConst(3);
+	// auto z2 = AD::makeConst(2);
+	// Eigen::Vector<ADP, 2> b = Eigen::Vector<ADP, 2>({
+	// 	{z1, z2}
+	//    });
+	//
+	// Eigen::Vector<ADP, 2> res = A * b;
+	//
+	// auto res0 = res(0);
+	// res0->forward();
+	// auto res1 = res(1);
+	// res1->forward();
+	//
+	// res(0)->gradient = 1;
+	// res(1)->gradient = 1;
+	// AD::backward(res(0));
+	// AD::backward(res(1));
+	// cout << res(0)->output.value() << endl;
+	// cout << x2->gradient.value() << endl;
 }
